9674023fa50a832165d9193ffc3d3a55675d41c9
joharbatta <joharbatta@gmail.com>
2020-10-01 02:38:42 +0530

    load products from backend

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const data=require('./data.js');
+const cors = require('cors');
+const app=express();
+app.use(cors());
+app.get("/api/products",(req,res)=>{
+    res.send(data.products);
+});
+
+app.listen(5000, () => {
+    console.log('serve at http://localhost:5000');
+});
