b056cffc6b28f574762e38cc174bfc4cccf24195
joharbatta <johar.batta@wakefit.co>
2020-11-21 23:33:30 +0530

    product screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ const routes = {
 };
 const router = async () => {
 	const request = parseRequestUrl();
+	console.log(request);
 	const parseUrl =
 		(request.resource ? `/${request.resource}` : '/') +
 		(request.id ? '/:id' : '') +
