6973c5bbf9547db819c6e3a11f7cda53de63aa3e
joharbatta <joharbatta@gmail.com>
2020-11-25 13:13:43 +0530

    login screen ui

---
+++ frontend/src/screens/SigninScreen.js
@@ -0,0 +1,35 @@
+const SigninScreen = {
+  after_render: () => {},
+  render: () => {
+    return `
+        <div class="form-container">
+        <form id="signin-form">
+            <ul class="form-items">
+                <li>
+                    <h1>Log In</h1>
+                </li>
+                <li>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" required/>
+                </li>
+                <li>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" required/>
+                </li>
+                <li>
+                     <button type="submit" class="primary">Signin</button>
+                </li>
+                <li>
+                    <div>
+                    New User?
+                    <a href="/#/register">Create your account </a>
+                    </div>
+                </li>
+            </ul>
+        </form>
+       
+        </div>`;
+  },
+};
+
+export default SigninScreen;
